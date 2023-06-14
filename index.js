32b9cf6a3806616284d67a279baa393d1affc7e7
Bahati019 <bahaticharity909@gmail.com>
2023-06-14 15:22:00 +0300

    Second commit

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ window.addEventListener('scroll',()=>{
     scroll.classList.toggle("scroll", window.scrollY > 0);
 })
  
+function sendEmail(e) {
+    e.preventDefault();
+
+    var name = document.forms["submit-to-google-sheet"]["Name"].value;
+    var email = document.forms["submit-to-google-sheet"]["Email"].value;
+    var message = document.forms["submit-to-google-sheet"]["Message"].value;
+
+    var subject = "Contact Form Submission";
+    var body = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+
+    var mailtoLink = "maito: charitybahati19@gmail.com" + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+    window.location.href = mailtoLink;
+}
